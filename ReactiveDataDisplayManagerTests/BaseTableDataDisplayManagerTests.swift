d21184605759b275883a2561dd8a79102a732c06
Alexander Filimonov <as_filimon@mail.ru>
2018-10-27 13:42:45 +0300

    Edited readme + added some tests

--- ReactiveDataDisplayManagerTests/BaseTableDataDisplayManagerTests.swift
+++ ReactiveDataDisplayManagerTests/BaseTableDataDisplayManagerTests.swift
@@ -75,6 +75,30 @@ final class BaseTableDataDisplayManagerTests: XCTestCase {
         XCTAssert(table.registerNibWasCalled)
     }
 
+    func testThatCustomOperationAddCellGeneratorCallsRegisterNib() {
+        // given
+        let headerGen = HeaderGenerator()
+        let gen = CellGenerator()
+        // when
+        ddm.addSectionHeaderGenerator(headerGen)
+        ddm += gen
+        // then
+        XCTAssert(table.registerNibWasCalled)
+    }
+
+    func testThatCustomOperationAddCellGeneratorsCallsRegisterNib() {
+        // given
+        let headerGen = HeaderGenerator()
+        let gen1 = CellGenerator()
+        let gen2 = CellGenerator()
+        // when
+        ddm.addSectionHeaderGenerator(headerGen)
+        ddm += [gen1, gen2]
+        // then
+        XCTAssert(table.registerNibWasCalled)
+        XCTAssert(ddm.cellGenerators[0].count == 2)
+    }
+
     func testThatAddCellGeneratorAddsEmptyHeaderIfThereIsNoCellHeaderGenerators() {
         // given
         let gen = CellGenerator()
