91ce6b2931a18205db3e2acf8dfdd5bd3ea8a483
Артемий Шабанов <shabanov@surfstudio.ru>
2019-02-25 10:55:25 +0300

    Fix a bug

--- Source/BaseTableDataDisplayManager/PaginableBaseTableDataDisplayManager.swift
+++ Source/BaseTableDataDisplayManager/PaginableBaseTableDataDisplayManager.swift
@@ -14,7 +14,7 @@ open class PaginableBaseTableDataDisplayManager: BaseTableDataDisplayManager {
     /// Called if table shows last cell
     public var lastCellShowingEvent = FutureEmptyEvent()
 
-    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    open override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.row == cellGenerators[indexPath.section].count - 1 {
             lastCellShowingEvent.invoke()
         }
