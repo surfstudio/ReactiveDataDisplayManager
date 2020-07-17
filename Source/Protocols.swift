ac55d5c6ef1374ea5075a9db94562d38852e78a3
Danil Komarov <patogenich@gmail.com>
2020-07-17 13:49:28 +0400

    DisplayableFlow was updated with new variable (`didEndDisplayCellEvent: BaseEvent<UITableViewCell>?`) to provide a better way to handle cell' life cycle events.
    (cherry picked from commit 920ead3ed67692f6b1422d4daffb319ed0b10f04)

--- Source/Protocols.swift
+++ Source/Protocols.swift
@@ -116,8 +116,13 @@ public protocol DisplayableFlow: class {
     /// Invokes when cell will displaying.
     var willDisplayEvent: BaseEvent<Void> { get }
 
+    /// SORTA DEPRECATED
     /// Invokes when cell did end displaying.
     var didEndDisplayEvent: BaseEvent<Void> { get }
+
+    /// Invokes when cell did end displaying. (Replacement for didEndDisplayEvent:; makes cell management easier.)
+    /// To be clear, it is just a workaround.
+    var didEndDisplayCellEvent: BaseEvent<UITableViewCell>? { get }
 
 }
 
