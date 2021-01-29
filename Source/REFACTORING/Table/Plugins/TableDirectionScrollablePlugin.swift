08542c251aadf5a471b66d0dde83f8d08db59eca
Anton Eysner <eysner@surfstudio.ru>
2021-01-29 14:27:17 +0200

    IDPT-611 added plugin with support for determining the direction of the scroll

---
+++ Source/REFACTORING/Table/Plugins/TableDirectionScrollablePlugin.swift
@@ -0,0 +1,9 @@
+//
+//  TableDirectionScrollablePlugin.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by Anton Eysner on 29.01.2021.
+//  Copyright © 2021 Александр Кравченков. All rights reserved.
+//
+
+import Foundation
