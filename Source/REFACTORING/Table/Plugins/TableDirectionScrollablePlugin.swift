fe764b0423b608a90bfcfcdfd95e835f3d363549
Anton Eysner <eysner@surfstudio.ru>
2021-01-29 14:27:58 +0200

    IDPT-611 added tableHeaderVisiblePlugin

--- Source/REFACTORING/Table/Plugins/TableDirectionScrollablePlugin.swift
+++ Source/REFACTORING/Table/Plugins/TableDirectionScrollablePlugin.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2021 Александр Кравченков. All rights reserved.
 //
 
-import Foundation
+/// Added support for determining the direction of the scroll
+public class TableDirectionScrollablePlugin: PluginAction<ScrollEvent, BaseTableStateManager> {
+
+    // MARK: - Properties
+
+    public private(set) var userDidScrolled = false
+    public var onScrollUp = BaseEmptyEvent()
+    public var onScrollDown = BaseEmptyEvent()
+
+    // MARK: - Private Properties
+
+    private var lastContentOffset: CGFloat = 0
+
+    // MARK: - PluginAction
+
+    override func process(event: ScrollEvent, with manager: BaseTableStateManager?) {
+        switch event {
+        case .willBeginDragging(let scrollView):
+            lastContentOffset = scrollView.contentOffset.y
+        case .didScroll(let scrollView):
+            if lastContentOffset > scrollView.contentOffset.y {
+                onScrollUp.invoke()
+            } else if lastContentOffset < scrollView.contentOffset.y {
+                onScrollDown.invoke()
+            }
+
+            if !userDidScrolled {
+                userDidScrolled = true
+            }
+        default:
+            break
+        }
+    }
+
+}
---
+++ Source/REFACTORING/Table/Plugins/TableHeaderVisiblePlugin.swift
@@ -0,0 +1,62 @@
+//
+//  TableHeaderVisiblePlugin.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by Anton Eysner on 29.01.2021.
+//  Copyright © 2021 Александр Кравченков. All rights reserved.
+//
+
+public class TableHeaderVisiblePlugin: PluginAction<TableEvent, BaseTableStateManager> {
+
+    // MARK: - Private Properties
+
+    private let action: (Int) -> Void
+
+    // MARK: - Initialization
+
+    public init(action: @escaping (Int) -> Void) {
+        self.action = action
+    }
+
+    // MARK: - PluginAction
+
+    override func process(event: TableEvent, with manager: BaseTableStateManager?) {
+        switch event {
+        case .willDisplayHeader(let section):
+            willDisplayHeader(with: section, manager: manager)
+        case .didEndDisplayHeader(let section):
+            didEndDisplayHeader(with: section, manager: manager)
+        default:
+            break
+        }
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension TableHeaderVisiblePlugin {
+
+    func willDisplayHeader(with section: Int, manager: BaseTableStateManager?) {
+        guard
+            let pathsForVisibleRows = manager?.tableView?.indexPathsForVisibleRows,
+            let firstPath = pathsForVisibleRows.first
+        else { return }
+
+        if firstPath.section == section {
+            action(firstPath.section)
+        }
+    }
+
+    func didEndDisplayHeader(with section: Int, manager: BaseTableStateManager?) {
+        guard
+            let pathsForVisibleRows = manager?.tableView?.indexPathsForVisibleRows,
+            let lastPath = pathsForVisibleRows.last
+        else { return }
+
+         if lastPath.section >= section {
+            action(section + 1)
+         }
+    }
+
+}
