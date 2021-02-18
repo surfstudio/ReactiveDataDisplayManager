9cda61b7142c9e8ddf1ea4479fa6ccd58f49b05f
Anton Eysner <eysner@surfstudio.ru>
2021-02-18 18:50:00 +0200

    maked manager and builder for stackView

---
+++ Source/REFACTORING/Stack/Stack+RDDM.swift
@@ -0,0 +1,43 @@
+//
+//  Stack+RDDM.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by Anton Eysner on 17.02.2021.
+//  Copyright © 2021 Александр Кравченков. All rights reserved.
+//
+
+import UIKit
+
+extension UIStackView: DataDisplayCompatible {}
+
+public extension DataDisplayWrapper where Base: UIStackView {
+
+    var baseBuilder: StackBuilder<BaseStackManager> {
+        StackBuilder(view: base, manager: BaseStackManager())
+    }
+
+}
+
+public class StackBuilder<T: BaseStackManager> {
+
+    // MARK: - Properties
+
+    let view: UIStackView
+    let manager: T
+
+    // MARK: - Initialization
+
+    init(view: UIStackView, manager: T) {
+        self.view = view
+        self.manager = manager
+    }
+
+    // MARK: - Public Methods
+
+    /// Build view and data display manager together and returns DataDisplayManager
+    public func build() -> T {
+        manager.view = view
+        return manager
+    }
+
+}
