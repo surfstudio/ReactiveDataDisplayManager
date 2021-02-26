06c5ab8893f4987288eb1fd69082c28939598ebf
Anton Eysner <74650281+eysnerSurf@users.noreply.github.com>
2021-02-26 13:23:51 +0200

    Add tests for stack manager (#104)
    * added tests for stack manager
    
    * fixes array name (#105)

--- Source/REFACTORING/Stack/Manager/BaseStackManager.swift
+++ Source/REFACTORING/Stack/Manager/BaseStackManager.swift
@@ -19,12 +19,12 @@ open class BaseStackManager: DataDisplayManager {
     // MARK: - Properties
 
     public weak var view: UIStackView!
-    public var cellGenerators: [StackCellGenerator]
+    public var generators: [StackCellGenerator]
 
     // MARK: - Initialization
 
     public init() {
-        cellGenerators = []
+        generators = []
     }
 
     // MARK: - DataDisplayManager
@@ -32,7 +32,7 @@ open class BaseStackManager: DataDisplayManager {
     public func forceRefill() {
         view.arrangedSubviews.forEach { $0.removeFromSuperview() }
 
-        cellGenerators.enumerated().forEach { [weak self] offset, generator in
+        generators.enumerated().forEach { [weak self] offset, generator in
             guard let stackView = self?.view else { return }
             let view = generator.generate(stackView: stackView, index: offset)
             stackView.addArrangedSubview(view)
@@ -40,36 +40,36 @@ open class BaseStackManager: DataDisplayManager {
     }
 
     public func addCellGenerator(_ generator: StackCellGenerator) {
-        cellGenerators.append(generator)
+        generators.append(generator)
     }
 
     public func addCellGenerators(_ generators: [StackCellGenerator], after: StackCellGenerator) {
         if let index = index(of: after) {
             generators.enumerated().forEach { offset, generator in
-                cellGenerators.insert(generator, at: index + offset + 1)
+                self.generators.insert(generator, at: index + offset + 1)
             }
         } else {
-            cellGenerators.append(contentsOf: generators)
+            self.generators.append(contentsOf: generators)
         }
     }
 
     public func addCellGenerator(_ generator: StackCellGenerator, after: StackCellGenerator) {
         if let index = index(of: after) {
-            cellGenerators.insert(generator, at: index + 1)
+            generators.insert(generator, at: index + 1)
         } else {
-            cellGenerators.append(generator)
+            generators.append(generator)
         }
     }
 
     public func addCellGenerators(_ generators: [StackCellGenerator]) {
-        cellGenerators.append(contentsOf: generators)
+        self.generators.append(contentsOf: generators)
     }
 
     public func update(generators: [StackCellGenerator]) {
         generators.forEach { generator in
             if let index = index(of: generator) {
-                cellGenerators.remove(at: index)
-                cellGenerators.insert(generator, at: index)
+                self.generators.remove(at: index)
+                self.generators.insert(generator, at: index)
             } else {
                 preconditionFailure("Can't find index in cellGenerators")
             }
@@ -77,7 +77,7 @@ open class BaseStackManager: DataDisplayManager {
     }
 
     public func clearCellGenerators() {
-        cellGenerators.removeAll()
+        generators.removeAll()
     }
 
 }
@@ -87,7 +87,7 @@ open class BaseStackManager: DataDisplayManager {
 private extension BaseStackManager {
 
     func index(of generator: StackCellGenerator) -> Int? {
-        return cellGenerators.firstIndex(where: { $0 === generator })
+        return generators.firstIndex(where: { $0 === generator })
     }
 
     func index(of view: UIView) -> Int? {
