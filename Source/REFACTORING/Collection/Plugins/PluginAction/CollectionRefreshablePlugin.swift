a25f445741387f4b4b58315ca40a4c6ef34ea9b2
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-03-03 14:03:24 +0400

    add refreshable plugin for collection

---
+++ Source/REFACTORING/Collection/Plugins/PluginAction/CollectionRefreshablePlugin.swift
@@ -0,0 +1,74 @@
+//
+//  CollectionRefreshablePlugin.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by Никита Коробейников on 03.03.2021.
+//  Copyright © 2021 Александр Кравченков. All rights reserved.
+//
+
+import UIKit
+
+/// Plugin to show and hide `refreshControl`
+public class CollectionRefreshablePlugin: BaseCollectionPlugin<ScrollEvent> {
+
+    // MARK: - Private Properties
+
+    private let refreshControl: UIRefreshControl
+    private weak var output: RefreshableOutput?
+
+    // MARK: - Initialization
+
+    /// - parameter refreshControl: closure with reaction to visibility of last cell
+    /// - parameter output: output signals to notify about refreshing
+    init(refreshControl: UIRefreshControl, with output: RefreshableOutput) {
+        self.refreshControl = refreshControl
+        self.output = output
+    }
+
+    // MARK: - BaseTablePlugin
+
+    public func setup(with manager: BaseTableManager?) {
+        manager?.view.refreshControl = refreshControl
+    }
+
+    public override func process(event: ScrollEvent, with manager: BaseCollectionManager?) {
+
+        switch event {
+        case .willEndDragging:
+            if refreshControl.isRefreshing {
+                output?.refreshContent(with: self)
+            }
+        default:
+            break
+        }
+    }
+
+}
+
+// MARK: - RefreshableInput
+
+extension CollectionRefreshablePlugin: RefreshableInput {
+
+    public func endRefreshing() {
+        refreshControl.endRefreshing()
+    }
+
+}
+
+// MARK: - Public init
+
+public extension BaseCollectionPlugin {
+
+    /// Plugin to show and hide `refreshControl`
+    ///
+    /// Show `refreshControl` on refreshing
+    /// Hide `refreshControl` when finish loading request
+    ///
+    /// - parameter refreshControl: closure with reaction to visibility of last cell
+    /// - parameter output: output signals to notify about refreshing
+    static func refreshable(refreshControl: UIRefreshControl,
+                            output: RefreshableOutput) -> CollectionRefreshablePlugin {
+        .init(refreshControl: refreshControl, with: output)
+    }
+
+}
