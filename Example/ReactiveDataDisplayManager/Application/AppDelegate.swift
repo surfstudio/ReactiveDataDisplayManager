1248ce216925d36c834f75d0ba229b68eb875fe8
Dmitry Korolev <startking@mail.ru>
2021-03-29 05:33:59 -0700

    IDPT-702 fix style

--- Example/ReactiveDataDisplayManager/Application/AppDelegate.swift
+++ Example/ReactiveDataDisplayManager/Application/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UIBarButtonItem.appearance().tintColor = UIColor.RDDMMainColor
         UITabBar.appearance().tintColor = UIColor.RDDMMainColor
+        UITableView.appearance().sectionIndexColor = UIColor.RDDMMainColor
         return true
     }
 
