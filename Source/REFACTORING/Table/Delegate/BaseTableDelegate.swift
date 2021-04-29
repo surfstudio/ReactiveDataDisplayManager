0fb0ccb560ac4254562223a4736e22755be77407
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-04-29 10:12:25 +0400

    Update Source/REFACTORING/Table/Delegate/BaseTableDelegate.swift
    Co-authored-by: Anton Eysner <74650281+eysnerSurf@users.noreply.github.com>

--- Source/REFACTORING/Table/Delegate/BaseTableDelegate.swift
+++ Source/REFACTORING/Table/Delegate/BaseTableDelegate.swift
@@ -13,7 +13,7 @@ open class BaseTableDelegate: NSObject, TableDelegate {
 
     // MARK: - Properties
 
-    weak public var manager: BaseTableManager?
+    public  weak var manager: BaseTableManager?
 
     public var estimatedHeight: CGFloat = 40
 
