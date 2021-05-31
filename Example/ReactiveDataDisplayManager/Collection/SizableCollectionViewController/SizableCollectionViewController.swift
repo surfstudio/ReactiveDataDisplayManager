0e85f83297816b7b42f243d34c3f07635dbe62d0
Anton Eysner <eysner@surfstudio.ru>
2021-05-31 08:52:56 +0200

    fixes linter issues

--- Example/ReactiveDataDisplayManager/Collection/SizableCollectionViewController/SizableCollectionViewController.swift
+++ Example/ReactiveDataDisplayManager/Collection/SizableCollectionViewController/SizableCollectionViewController.swift
@@ -59,9 +59,11 @@ private extension SizableCollectionViewController {
         let horizontalSectionInset = Constants.sectionInset.left + Constants.sectionInset.right
         let collectionWight = UIScreen.main.bounds.width - horizontalSectionInset
 
+        // swiftlint:disable line_length
         let sampleText = """
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         """
+        // swiftlint:enable line_length
 
         for _ in 0...300 {
             // Create viewModels for cell
