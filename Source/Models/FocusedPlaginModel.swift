d7bcc8cda333bf9284c9fbbbe930ec2d191f76c0
porohov <kombat_kos@mail.ru>
2021-11-17 17:33:44 +0400

    IDPT-737 Added comments for FocusedPlaginModel

--- Source/Models/FocusedPlaginModel.swift
+++ Source/Models/FocusedPlaginModel.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
+/// Model for FocusablePlugin
 public struct FocusedPlaginModel {
     let transform: CGAffineTransform?
     let transformDuration: CGFloat?
     let shadow: FocusedPlaginShadowModel?
 
+
+    /// Takes parameters
+    /// - Parameters:
+    ///     - transform:  defaul value nil
+    ///     - transformDuration:  defaul value CGSize(width: 0, height: 0)
+    ///     - shadow:  defaul value nil
     public init(transform: CGAffineTransform? = nil,
                 transformDuration: CGFloat? = 0.5,
                 shadow: FocusedPlaginShadowModel? = nil) {
@@ -21,12 +28,19 @@ public struct FocusedPlaginModel {
     }
 }
 
+/// Model for configure UIView shadow
 public struct FocusedPlaginShadowModel {
     let color: CGColor
     let offset: CGSize
     let opacity: Float
     let radius: CGFloat
 
+    /// Takes parameters
+    /// - Parameters:
+    ///     - color: UIColor
+    ///     - offset:  defaul value CGSize(width: 0, height: 0)
+    ///     - opacity:  defaul value 0.9
+    ///     - radius:  default value 10.0
     public init(
         color: UIColor,
         offset: CGSize = CGSize(width: 0, height: 0),
