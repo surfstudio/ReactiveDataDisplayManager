87db9ebf177bc03195bed8c165d33d8136461db0
porohov <kombat_kos@mail.ru>
2021-12-29 12:11:18 +0400

    added more comments

--- Source/Table/DataSource/TableSectionСhoice.swift
+++ Source/Table/DataSource/TableSectionСhoice.swift
@@ -8,7 +8,13 @@
 import Foundation
 
 public enum TableSectionСhoice {
+
+    /// Case for adding generators to a new section. The default header and footer are nil
     case newSection(header: TableHeaderGenerator? = nil, footer: TableFooterGenerator? = nil)
+
+    /// Case for adding generators to a section at a specified index
     case byIndex(Int)
+
+    /// Case for adding generators to the last section.
     case lastSection
 }
