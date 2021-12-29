b392cc195b7b4c50e6413ede0df591bd1ac3b426
porohov <kombat_kos@mail.ru>
2021-12-29 11:25:06 +0400

    Added comments in TableSectionsProvider, CollectionSectionsProvider

--- Source/Collection/DataSource/CollectionSectionsProvider.swift
+++ Source/Collection/DataSource/CollectionSectionsProvider.swift
@@ -12,8 +12,14 @@ open class CollectionSectionsProvider: SectionsProvider {
     public typealias HeaderGeneratorType = CollectionHeaderGenerator
     public typealias FooterGeneratorType = CollectionFooterGenerator
 
+    /// An array of sections. Within each section One header, One footer, and an array of generators
     open var sections: [Section<CollectionCellGenerator, CollectionHeaderGenerator, CollectionFooterGenerator>] = []
 
+    /// Adds generators
+    ///
+    /// - Parameters:
+    ///  - generators: - generators array
+    ///  - section: - enum CollectionSectionСhoice (allows you to choose which section to add generators to)
     func addCollectionGenerators(with generators: [CollectionCellGenerator], choice section: CollectionSectionСhoice) {
         switch section {
         case .newSection(let header, let footer):
@@ -27,9 +33,22 @@ open class CollectionSectionsProvider: SectionsProvider {
         }
     }
 
-    func addNewSection(section: Section<CollectionCellGenerator, CollectionHeaderGenerator, CollectionFooterGenerator>) {
-        sections.append(section)
+    /// Adds a new section with empty generator array and empty footer
+    func addHeader(header: CollectionHeaderGenerator) {
+        addCollectionGenerators(with: [], choice: .newSection(header: header, footer: nil))
     }
+
+    /// Replaces the empty footer of the last section
+    func addFooter(footer: CollectionFooterGenerator) {
+        let index = sections.count - 1
+        sections[index <= 0 ? 0 : index].footer = footer
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension CollectionSectionsProvider {
 
     func addGeneratorsInLastSection(generators: [CollectionCellGenerator]) {
         let index = sections.count - 1
@@ -46,15 +65,6 @@ open class CollectionSectionsProvider: SectionsProvider {
 
     func addGeneratorsInSection(by index: Int, generators: [CollectionCellGenerator]) {
         sections[index <= 0 ? 0 : index].generators.append(contentsOf: generators)
-    }
-
-    func addHeader(header: CollectionHeaderGenerator) {
-        addCollectionGenerators(with: [], choice: .newSection(header: header, footer: nil))
-    }
-
-    func addFooter(footer: CollectionFooterGenerator) {
-        let index = sections.count - 1
-        sections[index <= 0 ? 0 : index].footer = footer
     }
 
 }
