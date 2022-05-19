acb1b3194aa23409a00b1be6c09ab42c7400a067
Artem <kayumov@gmail.com>
2022-05-19 13:31:45 +0500

    Add UI test targets to project.yml

---
+++ Example/ReactiveDataDisplayManagerExampleUITests.swift
@@ -0,0 +1,18 @@
+//
+//  ReactiveDataDisplayManagerExampleUITests.swift
+//  
+//
+//  Created by Artem Kayumov on 19.05.2022.
+//
+
+import XCTest
+
+class ReactiveDataDisplayManagerExampleUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+        continueAfterFailure = false
+
+    }
+
+}
