faf5064f60836888392c9262503c513d2c147734
Konstantin Porokhov <56546669+kombatkos@users.noreply.github.com>
2022-06-08 13:33:34 +0400

    Update Source/Protocols/Plugins/PluginAction/PluginCollection.swift
    Co-authored-by: Nikita Korobeinikov <creepydodo@gmail.com>

--- Source/Protocols/Plugins/PluginAction/PluginCollection.swift
+++ Source/Protocols/Plugins/PluginAction/PluginCollection.swift
@@ -9,7 +9,7 @@
 /// Collection to store and execute `PluginAction`
 public struct PluginCollection<Plugin: PluginAction> {
 
-    var plugins = [Plugin]()
+    private(set) var plugins = [Plugin]()
 
     /// Add plugin to collection
     ///
