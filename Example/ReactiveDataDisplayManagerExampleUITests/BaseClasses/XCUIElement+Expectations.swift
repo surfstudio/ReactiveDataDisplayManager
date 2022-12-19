48c6b6de8bea63d7264081aa7f102508cdefab28
Nikita Korobeinikov <korobeinikov@surf.dev>
2022-12-19 21:20:59 +0400

    make UI tests for refresh control more stable

---
+++ Example/ReactiveDataDisplayManagerExampleUITests/BaseClasses/XCUIElement+Expectations.swift
@@ -0,0 +1,21 @@
+//
+//  XCUIElement+Expectations.swift
+//  ReactiveDataDisplayManagerExampleUITests
+//
+//  Created by Никита Коробейников on 19.12.2022.
+//
+
+import XCTest
+
+extension XCUIElement {
+
+    func waitForNonExistence(timeout: TimeInterval) -> Bool {
+        let predicate   = NSPredicate(format: "exists == false")
+        let expectation = XCTNSPredicateExpectation(predicate: predicate, object: self)
+
+        let result = XCTWaiter().wait(for: [expectation], timeout: timeout)
+
+        return result == .completed
+    }
+
+}
