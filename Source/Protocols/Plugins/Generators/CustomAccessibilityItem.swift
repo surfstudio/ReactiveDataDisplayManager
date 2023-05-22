c1268a3eba1d845972607cd6da90ebd3c074192e
Sergey Korshunov <sergey.korshunov@devexperts.com>
2023-05-22 19:08:29 +0300

    added accessibility plugin

---
+++ Source/Protocols/Plugins/Generators/CustomAccessibilityItem.swift
@@ -0,0 +1,13 @@
+//
+//  CustomAccessibilityItem.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by korshunov on 22.05.2023.
+//
+
+import UIKit
+
+public protocol CustomAccessibilityItem {
+    func setupAccessibility()
+}
+
