31383baac13abf1708410fa6310293ef864122f9
Sergey Korshunov <sergey.korshunov@devexperts.com>
2023-05-24 11:46:13 +0300

    added accessibility strategies

--- Source/Collection/Plugins/PluginAction/CollectionAccessibilityPlugin.swift
+++ Source/Collection/Plugins/PluginAction/CollectionAccessibilityPlugin.swift
@@ -12,33 +12,14 @@ final class CollectionAccessibilityPlugin: BaseCollectionPlugin<CollectionEvent>
     override func process(event: CollectionEvent, with manager: BaseCollectionManager?) {
         switch event {
         case .willDisplayCell(let indexPath, let cell):
-            if let customItem = cell as? CustomAccessibilityItem {
-                customItem.setupAccessibility()
-            } else {
-                AccessibilityParser.processAndApply(process: cell.contentView, applyTo: cell)
-                if let generator = manager?.generators[indexPath.section][indexPath.row],
-                    generator is SelectableItem || generator is FoldableItem {
-                    cell.accessibilityTraits.insert(.button)
-                }
+            let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityItem
+            if let accessibilityItem = generator ?? cell as? AccessibilityItem {
+                accessibilityItem.modifierType.modify(view: cell.contentView, with: accessibilityItem)
             }
-        case .willDisplaySupplementaryView(let indexPath, let view, let elementKind):
-            if let customItem = view as? CustomAccessibilityItem {
-                customItem.setupAccessibility()
-            } else {
-                AccessibilityParser.processAndApply(process: view, applyTo: view)
-                var generator: Any?
-                switch elementKind {
-                case UICollectionView.elementKindSectionHeader:
-                    generator = manager?.sections[indexPath.section]
-                    view.accessibilityTraits.insert(.header)
-                case UICollectionView.elementKindSectionFooter:
-                    generator = manager?.footers[indexPath.section]
-                default:
-                    break
-                }
-                if let generator = generator, generator is SelectableItem || generator is FoldableItem {
-                    view.accessibilityTraits.insert(.button)
-                }
+        case .willDisplaySupplementaryView(let indexPath, let view, _):
+            let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityItem
+            if let accessibilityItem = generator ?? view as? AccessibilityItem {
+                accessibilityItem.modifierType.modify(view: view, with: accessibilityItem)
             }
         default:
             break
