8414389a88146509ce5df6a38a3619eb2bd8f943
Sergey Korshunov <sergey.korshunov@devexperts.com>
2023-05-24 11:46:13 +0300

    added accessibility strategies

---
+++ Source/Protocols/Plugins/Generators/AccessibilityItem.swift
@@ -0,0 +1,90 @@
+//
+//  AccessibilityItem.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by korshunov on 22.05.2023.
+//
+
+import UIKit
+
+public enum AccessibilityStrategy {
+    case ignored
+    case just(String?)
+    case from(NSObject, keyPath: KeyPath<NSObject, String?> = \.accessibilityLabel)
+    indirect case joined([AccessibilityStrategy], separator: String)
+
+    var isIgnored: Bool {
+        if case .ignored = self {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    var value: String? {
+        switch self {
+        case .ignored:
+            return nil
+        case .just(let string):
+            return string
+        case .from(let object, let keyPath):
+            return object[keyPath: keyPath]
+        case .joined(let cases, let separator):
+            return cases.compactMap(\.value).joined(separator: separator)
+        }
+    }
+}
+
+public enum AccessibilityTraitsStrategy {
+    case ignored
+    case just(UIAccessibilityTraits)
+    case from(NSObject)
+    case merge([NSObject])
+
+    var isIgnored: Bool {
+        if case .ignored = self {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    var value: UIAccessibilityTraits? {
+        switch self {
+        case .ignored:
+            return nil
+        case .just(let traits):
+            return traits
+        case .from(let object):
+            return object.accessibilityTraits
+        case .merge(let objects):
+            return objects.map(\.accessibilityTraits).reduce(UIAccessibilityTraits(), { $0.union($1) })
+        }
+    }
+
+//    mutating func insert(traits: UIAccessibilityTraits) {
+//        self = .just(value.map { $0.union(traits) } ?? traits)
+//    }
+//
+//    mutating func remove(traits: UIAccessibilityTraits) {
+//        self = .just(value.map { $0.subtracting(traits) } ?? traits)
+//    }
+}
+
+public protocol AccessibilityItem {
+    typealias AccessibilityModifierType = AccessibilityModifier.Type
+    var modifierType: AccessibilityModifierType { get }
+
+    var labelStrategy: AccessibilityStrategy { get }
+    var valueStrategy: AccessibilityStrategy { get }
+    var traitsStrategy: AccessibilityTraitsStrategy { get }
+}
+
+public extension AccessibilityItem {
+    var parcerType: AccessibilityModifierType { DefaultAccessibilityModifier.self }
+    
+    var labelStrategy: AccessibilityStrategy { .ignored }
+    var valueStrategy: AccessibilityStrategy { .ignored }
+    var traitsStrategy: AccessibilityTraitsStrategy { .ignored }
+}
+
