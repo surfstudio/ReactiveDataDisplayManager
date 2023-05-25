f5e97c5d41686fcfa74d8cff78c516affed3e080
Anton Golubeykov <ds6580@yandex.ru>
2023-05-25 13:54:16 +0300

    Changed implementation to TableLabelCell.rddm.baseGenerator

--- Example/ReactiveDataDisplayManager/Table/ComponentsOverviewTableViewController/ComponentsOverviewTableViewController.swift
+++ Example/ReactiveDataDisplayManager/Table/ComponentsOverviewTableViewController/ComponentsOverviewTableViewController.swift
@@ -39,7 +39,7 @@ final class ComponentsOverviewTableViewController: UIViewController {
                                                   style: sentMessageTimeStyle,
                                                   layout: sentMessageTimeLayout,
                                                   edgeInsets: UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 0))
-    private lazy var sentMessageTimeGenerator = BaseCellGenerator<TableLabelCell>(with: sentMessageTimeModel, registerType: .class)
+    private lazy var sentMessageTimeGenerator = TableLabelCell.rddm.baseGenerator(with: sentMessageTimeModel, and: .class)
 
     private lazy var recievedTimeMessageStyle = TextStyle(color: .gray, font: .systemFont(ofSize: 12, weight: .light))
     private lazy var recievedTimeMessageLayout = TextLayout(alignment: .right, lineBreakMode: .byWordWrapping, numberOfLines: 0)
@@ -47,7 +47,7 @@ final class ComponentsOverviewTableViewController: UIViewController {
                                                       style: recievedTimeMessageStyle,
                                                       layout: recievedTimeMessageLayout,
                                                       edgeInsets: UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 16))
-    private lazy var recievedMessageTimeGenerator = BaseCellGenerator<TableLabelCell>(with: recievedTimeMessageModel, registerType: .class)
+    private lazy var recievedMessageTimeGenerator = TableLabelCell.rddm.baseGenerator(with: recievedTimeMessageModel, and: .class)
 
     private lazy var dateStyle = TextStyle(color: .black, font: .systemFont(ofSize: 12, weight: .light))
     private lazy var dateLayout = TextLayout(alignment: .center, lineBreakMode: .byWordWrapping, numberOfLines: 0)
@@ -55,7 +55,7 @@ final class ComponentsOverviewTableViewController: UIViewController {
                                        style: dateStyle,
                                        layout: dateLayout,
                                        edgeInsets: UIEdgeInsets(top: 0, left: 0, bottom: 12, right: 0))
-    private lazy var dateGenerator = BaseCellGenerator<TableLabelCell>(with: dateModel, registerType: .class)
+    private lazy var dateGenerator = TableLabelCell.rddm.baseGenerator(with: dateModel, and: .class)
 
     private lazy var sentMessageStyle = TextStyle(color: .black, font: .systemFont(ofSize: 16, weight: .regular))
     private lazy var sentMessageLayout = TextLayout(alignment: .left, lineBreakMode: .byWordWrapping, numberOfLines: 0)
@@ -63,7 +63,7 @@ final class ComponentsOverviewTableViewController: UIViewController {
                                                                style: sentMessageStyle,
                                                                layout: sentMessageLayout,
                                                                edgeInsets: UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 0))
-    private lazy var sentMessageGenerator = BaseCellGenerator<TableLabelCell>(with: sentMessageModel, registerType: .class)
+    private lazy var sentMessageGenerator = TableLabelCell.rddm.baseGenerator(with: sentMessageModel, and: .class)
 
     private lazy var recievedMessageStyle = TextStyle(color: .black, font: .systemFont(ofSize: 16, weight: .regular))
     private lazy var recievedMessageLayout = TextLayout(alignment: .right, lineBreakMode: .byWordWrapping, numberOfLines: 0)
@@ -71,7 +71,7 @@ final class ComponentsOverviewTableViewController: UIViewController {
                                                   style: recievedMessageStyle,
                                                   layout: recievedMessageLayout,
                                                   edgeInsets:  UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 16))
-    private lazy var recievedMessageGenerator = BaseCellGenerator<TableLabelCell>(with: recievedMessageModel, registerType: .class)
+    private lazy var recievedMessageGenerator = TableLabelCell.rddm.baseGenerator(with: recievedMessageModel, and: .class)
 
     // MARK: - UIViewController
 
