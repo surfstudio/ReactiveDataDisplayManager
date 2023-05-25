b1c894b01dbe7133c8a81c6a117fb6a64807a40f
Sergey Korshunov <sergey.korshunov@devexperts.com>
2023-05-25 12:58:52 +0300

    renamed protocols

--- Source/Utils/AccessibilityModifier.swift
+++ Source/Utils/AccessibilityModifier.swift
@@ -9,25 +9,25 @@ import UIKit
 
 
 public protocol AccessibilityModifier {
-    static func modify(view: UIView, with cell: AccessibilityCell, generator: AccessibilityItem?)
+    static func modify(view: UIView, with item: AccessibilityItem, generator: AccessibilityStrategyProvider?)
 }
 
 enum DefaultAccessibilityModifier: AccessibilityModifier {
-    static func modify(view: UIView, with cell: AccessibilityCell, generator: AccessibilityItem?) {
+    static func modify(view: UIView, with item: AccessibilityItem, generator: AccessibilityStrategyProvider?) {
         view.isAccessibilityElement = true
 
-        if !cell.labelStrategy.isIgnored || generator?.labelStrategy.isIgnored == false {
-            view.accessibilityLabel = cell.accessibilityStrategyConflictResolver(cellStrategy: cell.labelStrategy,
+        if !item.labelStrategy.isIgnored || generator?.labelStrategy.isIgnored == false {
+            view.accessibilityLabel = item.accessibilityStrategyConflictResolver(itemStrategy: item.labelStrategy,
                                                                                  generatorStrategy: generator?.labelStrategy)
         }
 
-        if !cell.valueStrategy.isIgnored || generator?.valueStrategy.isIgnored == false {
-            view.accessibilityValue = cell.accessibilityStrategyConflictResolver(cellStrategy: cell.valueStrategy,
+        if !item.valueStrategy.isIgnored || generator?.valueStrategy.isIgnored == false {
+            view.accessibilityValue = item.accessibilityStrategyConflictResolver(itemStrategy: item.valueStrategy,
                                                                                  generatorStrategy: generator?.valueStrategy)
         }
 
-        if !cell.traitsStrategy.isIgnored || generator?.traitsStrategy.isIgnored == false {
-            let traits = [cell.traitsStrategy.value, generator?.traitsStrategy.value]
+        if !item.traitsStrategy.isIgnored || generator?.traitsStrategy.isIgnored == false {
+            let traits = [item.traitsStrategy.value, generator?.traitsStrategy.value]
                 .compactMap { $0 }
                 .reduce(UIAccessibilityTraits(), { $0.union($1) })
             view.accessibilityTraits = traits
