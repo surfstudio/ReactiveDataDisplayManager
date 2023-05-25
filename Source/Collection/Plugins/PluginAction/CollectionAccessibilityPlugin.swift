ba6d2b7b9b7cffd4c2d706fcb24bbbaa25008c54
Sergey Korshunov <sergey.korshunov@devexperts.com>
2023-05-25 12:58:52 +0300

    renamed protocols

--- Source/Collection/Plugins/PluginAction/CollectionAccessibilityPlugin.swift
+++ Source/Collection/Plugins/PluginAction/CollectionAccessibilityPlugin.swift
@@ -12,16 +12,16 @@ final class CollectionAccessibilityPlugin: BaseCollectionPlugin<CollectionEvent>
     override func process(event: CollectionEvent, with manager: BaseCollectionManager?) {
         switch event {
         case .willDisplayCell(let indexPath, let cell):
-            if let accessibilityItem = cell as? AccessibilityCell {
-                let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityItem
+            if let accessibilityItem = cell as? AccessibilityItem {
+                let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityStrategyProvider
                 accessibilityItem.modifierType.modify(view: cell.contentView, with: accessibilityItem, generator: generator)
             }
         case .willDisplaySupplementaryView(let indexPath, let view, let kind):
             guard [UICollectionView.elementKindSectionHeader, UICollectionView.elementKindSectionFooter].contains(kind) else {
                 return
             }
-            if let accessibilityItem = view as? AccessibilityCell {
-                let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityItem
+            if let accessibilityItem = view as? AccessibilityItem {
+                let generator = manager?.generators[indexPath.section][indexPath.row] as? AccessibilityStrategyProvider
                 accessibilityItem.modifierType.modify(view: view, with: accessibilityItem, generator: generator)
             }
         default:
