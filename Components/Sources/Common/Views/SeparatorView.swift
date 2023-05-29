62cf08d8ba3f10a1b628ff83b6560469d3b19a8c
Anton Golubeykov <47087482+Golubeykov@users.noreply.github.com>
2023-05-29 12:07:23 +0300

    Update Components/Sources/Common/Views/SeparatorView.swift
    Co-authored-by: Nikita Korobeinikov <korobeinikov@surf.dev>

--- Components/Sources/Common/Views/SeparatorView.swift
+++ Components/Sources/Common/Views/SeparatorView.swift
@@ -33,7 +33,7 @@ public class SeparatorView: UIView {
 
 extension SeparatorView: ConfigurableItem {
 
-    public struct Model {
+    public struct Model: Equatable {
         public let height: CGFloat
         public let color: UIColor?
         public let edgeInsets: UIEdgeInsets
