2051b96ffd58517155db706e7b2f91b600d49129
Sergey Korshunov <korshunov@surf.dev>
2023-05-31 13:17:08 +0300

    update structure

---
+++ Source/Protocols/Plugins/Generators/Accessibility/AccessibilityItem+Providers.swift
@@ -0,0 +1,8 @@
+//
+//  AccessibilityItem+Providers.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by korshunov on 31.05.2023.
+//
+
+import Foundation
---
+++ Source/Protocols/Plugins/Generators/Accessibility/AccessibilityItem+Strategies.swift
@@ -0,0 +1,85 @@
+//
+//  AccessibilityStrategies.swift
+//  ReactiveDataDisplayManager
+//
+//  Created by korshunov on 31.05.2023.
+//
+
+import UIKit
+
+/// Default accessibility strategy for string value
+public enum AccessibilityStringStrategy {
+
+    /// value will not be change by this strategy
+    case ignored
+
+    /// simple string value, can be nil
+    case just(String?)
+
+    /// a reference value from object. By default it's `accessibilityLabel`
+    /// - parameter keyPath: specified keypath from value would be taken
+    case from(object: NSObject, keyPath: KeyPath<NSObject, String?> = \.accessibilityLabel)
+
+    /// a combination of strategies
+    indirect case joined([AccessibilityStringStrategy])
+
+    var isIgnored: Bool {
+        if case .ignored = self {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    var value: String? {
+        switch self {
+        case .ignored:
+            return nil
+        case .just(let string):
+            return string
+        case .from(let object, let keyPath):
+            return object[keyPath: keyPath]
+        case .joined(let strategies):
+            return strategies.compactMap(\.value).joined()
+        }
+    }
+}
+
+/// Accessibility strategy for `UIAccessibilityTraits` parameter
+public enum AccessibilityTraitsStrategy {
+
+    /// value will not be change by this strategy
+    case ignored
+
+    /// simple accessibility traits
+    case just(UIAccessibilityTraits)
+
+    /// a reference traits from another object
+    case from(object: NSObject)
+
+    /// a reference traits merged from another objects
+    case merge([NSObject])
+
+    var isIgnored: Bool {
+        if case .ignored = self {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    /// accessibility traits of current strategy
+    /// - Returns: nil if `isIgnored`, otherwise `UIAccessibilityTraits`
+    var value: UIAccessibilityTraits? {
+        switch self {
+        case .ignored:
+            return nil
+        case .just(let traits):
+            return traits
+        case .from(let object):
+            return object.accessibilityTraits
+        case .merge(let objects):
+            return objects.map(\.accessibilityTraits).reduce(UIAccessibilityTraits(), { $0.union($1) })
+        }
+    }
+}
